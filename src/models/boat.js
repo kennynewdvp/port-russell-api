e2e67bb3f27c9e9d301cf722aa439509528a3ce0
kenny <kennycombe72400@outlook.fr>
2026-01-22 13:07:09 +0100

    feat: modèle et routes API pour les bateaux validés

---
+++ src/models/boat.js
@@ -0,0 +1,30 @@
+/**
+ * @module models/Boat
+ * @description Modèle Mongoose pour représenter un bateau au port
+ */
+const mongoose = require('mongoose');
+
+const boatSchema = new mongoose.Schema({
+    name: { 
+        type: String, 
+        required: [true, "Le nom du bateau est obligatoire"] 
+    },
+    owner: { 
+        type: String, 
+        required: [true, "Le nom du propriétaire est obligatoire"] 
+    },
+    length: { 
+        type: Number, 
+        required: [true, "La longueur est obligatoire"] 
+    },
+    description: { 
+        type: String,
+        default: "Aucune description" 
+    },
+    arrivalDate: { 
+        type: Date, 
+        default: Date.now 
+    }
+});
+
+module.exports = mongoose.model('Boat', boatSchema);
